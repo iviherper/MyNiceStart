3394209f2002a584012d048c17866a3a763ba751
Ivan <iherper97@hotmail.com>
2021-02-03 18:13:27 +0100

    Readme.md

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,20 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imgGoogle;
+    TextView menucontext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imgGoogle=findViewById(R.id.imgGoogle);
+        menucontext = findViewById(R.id.menucontext);
+        registerForContextMenu(menucontext);
         Glide.with(this).load("https://www.pinclipart.com/picdir/big/87-878560_drawing-tigers-vector-dibujos-de-tigres-blanco-y.png").into(imgGoogle);
         Button btnGo = (Button) findViewById(R.id.buttongo);
         btnGo.setOnClickListener(new View.OnClickListener() {
@@ -28,11 +36,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
 
             }
-
         });
+    }
 
-
-
-
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)
+                item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.item:
+                Toast toast = Toast.makeText(this, "going ITEM 1",
+                        Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+            case R.id.action_settings:
+                Toast toast2 = Toast.makeText(this, "going ITEM 2",
+                        Toast.LENGTH_LONG);
+                toast2.show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -1,14 +1,41 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    BottomAppBar botonappbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        botonappbar = findViewById(R.id.bottom_app_bar);
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity2.this, "FAB Clicked.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        botonappbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity2.this, "Menu Clicked.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        botonappbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(MainActivity2.this, item.getTitle()+" Clicked", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
